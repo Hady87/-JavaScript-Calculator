e08e3e32b4a4aae4c463ce50f214a2660d0c7661
Hady <hady.etd@gmail.com>
2022-03-09 22:06:02 -0500

    After calcul

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+
+//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {CalculatorProvider} from "./context/CalculatorContext"
 import MainComponent from "./components/MainComponent";
 
 
 function App() {
   return (
     <>
+    <CalculatorProvider>
       <MainComponent/>
+      </CalculatorProvider>
     </>
   );
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,130 +1,150 @@
-import React from "react";
+import React, { useContext } from "react";
+import CalculatorContext from "../context/CalculatorContext";
 
 function MainComponent() {
+  const { result, display, handleResult, handleDisplay, clear } =
+    useContext(CalculatorContext);
+
   return (
     <>
-      <div className="container w-25 gx-0">
-        <div className="row gx-0">
-          <div className="col col-12" id="display">
-            <p>display</p>
-          </div>
-          </div>
+      <div className="container w-25">
         <div className="wrapper">
+          <div className=" d-flex align-items-end flex-column" id="display">
+            {result ? <p>{`${display}  ${result}`}</p> : <p> {display || 0}</p>}
+            <p>
+              <strong>{result || 0}</strong>
+            </p>
+          </div>
           <input
-              type="button"
-              className="btn btn-danger btn-lg "
-              value="AC"
-              id="clear"
-            />
+            type="button"
+            className="btn btn-danger btn-lg "
+            value="AC"
+            id="clear"
+            onClick={clear}
+          />
 
-            <input
-              type="button"
-              className="btn btn-secondary btn-lg "
-              value="/"
-              id="divide"
-            />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="/"
+            id="divide"
+            onClick={() => handleDisplay("/")}
+          />
 
-            <input
-              type="button"
-              className="btn btn-secondary btn-lg"
-              value="x"
-              id="multiply"
-            />
-             <input
-              type="button"
-              className="btn btn-secondary btn-lg "
-              value="7"
-              id="seven"
-            />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg"
+            value="x"
+            id="multiply"
+            onClick={() => handleDisplay("*")}
+          />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="7"
+            id="seven"
+            onClick={() => handleDisplay("7")}
+          />
 
-            <input
-              type="button"
-              className="btn btn-secondary btn-lg "
-              value="8"
-              id="eight"
-            />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="8"
+            id="eight"
+            onClick={() => handleDisplay("8")}
+          />
 
-            <input
-              type="button"
-              className="btn btn-secondary btn-lg "
-              value="9"
-              id="nine"
-            />
-            <input
-              type="button"
-              className="btn btn-secondary btn-lg "
-              value="-"
-              id="subtract"
-            />
-             <input
-              type="button"
-              className="btn btn-secondary btn-lg "
-              value="4"
-              id="four"
-            />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="9"
+            id="nine"
+            onClick={() => handleDisplay("9")}
+          />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="-"
+            id="subtract"
+            onClick={() => handleDisplay("-")}
+          />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="4"
+            id="four"
+            onClick={() => handleDisplay("4")}
+          />
 
-            <input
-              type="button"
-              className="btn btn-secondary btn-lg "
-              value="5"
-              id="five"
-            />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="5"
+            id="five"
+            onClick={() => handleDisplay("5")}
+          />
 
-            <input
-              type="button"
-              className="btn btn-secondary btn-lg "
-              value="6"
-              id="six"
-            />
-            <input
-              type="button"
-              className="btn btn-secondary btn-lg "
-              value="+"
-              id="add"
-            />
-             <input
-                type="button"
-                className="btn btn-secondary btn-lg "
-                value="1"
-                id="one"
-              />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="6"
+            id="six"
+            onClick={() => handleDisplay("6")}
+          />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="+"
+            id="add"
+            onClick={() => handleDisplay("+")}
+          />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="1"
+            id="one"
+            onClick={() => handleDisplay("1")}
+          />
 
-              <input
-                type="button"
-                className="btn btn-secondary btn-lg "
-                value="2"
-                id="two"
-              />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="2"
+            id="two"
+            onClick={() => handleDisplay("2")}
+          />
 
-              <input
-                type="button"
-                className="btn btn-secondary btn-lg "
-                value="3"
-                id="three"
-              />
-                <input
-              type="button"
-              className="btn btn-secondary btn-lg "
-              value="="
-              id="equals"
-            />
-             <input
-                type="button"
-                className="btn btn-secondary btn-lg "
-                value="0"
-                id="zero"
-              />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="3"
+            id="three"
+            onClick={() => handleDisplay("3")}
+          />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="="
+            id="equals"
+            onClick={handleResult}
+          />
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg "
+            value="0"
+            id="zero"
+            onClick={() => handleDisplay("0")}
+          />
 
-              <input
-                type="button"
-                className="btn btn-secondary btn-lg"
-                value="."
-                id="decimal"
-              />
-            
+          <input
+            type="button"
+            className="btn btn-secondary btn-lg"
+            value="."
+            id="decimal"
+            onClick={() => handleDisplay(".")}
+          />
         </div>
       </div>
-      ;
     </>
   );
 }
